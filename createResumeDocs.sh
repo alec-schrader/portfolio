c94a9a3b156c73165446a7dd53d7a0f009222a5f
Alec Schrader <alecschrader@gmail.com>
2024-02-14 17:12:45 -0500

    create new docx reference

--- createResumeDocs.sh
+++ createResumeDocs.sh
@@ -1,3 +1,3 @@
 pandoc -o public/assets/alec_schrader_resume.html -c resume-css-stylesheet.css resume.md
 wkhtmltopdf public/assets/alec_schrader_resume.html public/assets/alec_schrader_resume.pdf 
-pandoc -o public/assets/alec_schrader_resume.docx --reference-doc=resume-docx-reference.docx resume.md
+pandoc -o public/assets/alec_schrader_resume.docx --reference-doc=custom-reference.docx resume.md
