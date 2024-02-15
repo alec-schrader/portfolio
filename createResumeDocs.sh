223dc0fda0a88fffda9d77d8a3abb5ccf72e4fa3
Alec Schrader <alecschrader@gmail.com>
2024-02-15 13:50:44 -0500

    Edit pdf generation and styles

--- createResumeDocs.sh
+++ createResumeDocs.sh
@@ -1,3 +1,5 @@
-pandoc -o public/assets/alec_schrader_resume.html -c resume-css-stylesheet.css resume.md
-wkhtmltopdf public/assets/alec_schrader_resume.html public/assets/alec_schrader_resume.pdf 
-pandoc -o public/assets/alec_schrader_resume.docx --reference-doc=custom-reference.docx resume.md
+pandoc -o temphtml.html -c pdfstyles.css -s resume.md
+pandoc -o public/assets/alec_schrader_resume.html resume.md
+wkhtmltopdf --enable-local-file-access temphtml.html public/assets/alec_schrader_resume.pdf 
+pandoc -o public/assets/alec_schrader_resume.docx --reference-doc=docstyles.docx resume.md
+rm temphtml.html
